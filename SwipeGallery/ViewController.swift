f87ce223bb7940c77bba9c2ce210e97874788920
Jilin Liu <jilin@oomba.com>
2018-05-30 12:29:20 -0700

    add thumb up and down animation

--- SwipeGallery/ViewController.swift
+++ SwipeGallery/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var thumbImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -22,9 +23,19 @@ class ViewController: UIViewController {
         let point = sender.translation(in: card)
         card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
         
+        let xFromCenter = card.center.x - view.center.x
+        if xFromCenter > 0 { // Swipe to the right
+            thumbImageView.image = #imageLiteral(resourceName: "thumb-up")
+        } else {
+            thumbImageView.image = #imageLiteral(resourceName: "thumb-down")
+        }
+        
+        thumbImageView.alpha = abs(xFromCenter)/view.center.x
+        
         if sender.state == .ended {
             UIView.animate(withDuration: 0.2, animations: {
                 card.center = self.view.center
+                self.thumbImageView.alpha = 0
             })
         }
     }
