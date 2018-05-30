430286c0ee0e1ca8af56e430435cf6d7a60ea998
Jilin Liu <jilin@oomba.com>
2018-05-29 23:43:13 -0700

    add readme file, configure carthage, import Log library

---
+++ README.md
@@ -0,0 +1,19 @@
+# SwipeGallery iOS App
+
+## Carthage
+
+This project uses Carthage: [Carthage on Github](https://github.com/Carthage/Carthage)
+
+Download dependencies using: 
+
+`carthage bootstrap --platform iOS --no-use-binaries`
+
+Add dependencies:
+
+- Update Cartfile with dependency (see github page for further details)
+- Lock in dependency versions: `carthage update --platform iOS --no-use-binaries`
+- Under SwipeGallery > General > Linked Frameworks and Libraries, add [framework-name].framework
+
+## Getting Started
+
+Build and run the DragBoard target.
--- SwipeGallery/ViewController.swift
+++ SwipeGallery/ViewController.swift
@@ -12,14 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
+        guard let card = sender.view else {
+            Log.logger.error("No attached view to tap gesture")
+        }
+        
     }
-
-
 }
 
