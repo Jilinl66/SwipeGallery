10448fdc25389a8783ed5bcb3333d604e79d24cd
Jilin Liu <jilin@oomba.com>
2018-06-01 15:58:40 -0700

    set up drawing page view class and storyboard

---
+++ SwipeGallery/DrawView.swift
@@ -0,0 +1,16 @@
+//
+//  DrawView.swift
+//  SwipeGallery
+//
+//  Created by Jilin Liu on 6/1/18.
+//  Copyright © 2018 JilinStudio, Inc. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
